61cc3719beafb446f80f31dce6c3a5453b33158d
diayan <diayans@outlook.com>
2022-04-11 10:01:09 +0000

    Initial Commit

---
+++ readion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  readion
+//
+//  Created by diayan siat on 11/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ readion/readionApp.swift
@@ -0,0 +1,17 @@
+//
+//  readionApp.swift
+//  readion
+//
+//  Created by diayan siat on 11/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct readionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
