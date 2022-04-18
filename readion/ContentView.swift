c867aee501c84769d983d07d1ba69593f087c3d2
diayan <diayans@outlook.com>
2022-04-18 00:25:58 +0000

    generate random data and persist in core data

--- readion/ContentView.swift
+++ readion/ContentView.swift
@@ -27,12 +27,27 @@ struct PushButton: View {
 }
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
     @FetchRequest(sortDescriptors: []) var students: FetchedResults<Student>
     
      var body: some View {
          VStack {
              List(students) { student in
                  Text(student.name ?? "Unknown")
+             }
+             
+             Button("Add") {
+                 let firstNames = ["Ginny", "Harry", "Hermoine", "Luna", "Ron"]
+                 let lastNames = ["Granger", "Lovel", "Potter", "Weasley"]
+                 
+                 let chosenFirstName = firstNames.randomElement()!
+                 let chosenLastName = lastNames.randomElement()!
+                 
+                let student = Student(context: moc)
+                 student.id = UUID()
+                 student.name = "\(chosenFirstName) \(chosenLastName)"
+                 
+                 try? moc.save()
              }
          }
     }
